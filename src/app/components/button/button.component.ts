2d7965dea66f66561ad3d8e47fe0771a2103e692
Elee Lawleit <alibutt200235@gmail.com>
2024-04-01 10:57:11 +0500

    passing props in angular

--- src/app/components/button/button.component.ts
+++ src/app/components/button/button.component.ts
@@ -1,12 +1,16 @@
-import { Component } from '@angular/core';
+import { Component, Input } from '@angular/core';
+import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'app-button',
   standalone: true,
-  imports: [],
+  imports: [CommonModule],
   templateUrl: './button.component.html',
   styleUrl: './button.component.css'
 })
 export class ButtonComponent {
+
+  @Input() text: string = ""
+  @Input() color: string = ""
 
 }
