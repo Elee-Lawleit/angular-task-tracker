111882f0bf7d530ed368c3ec94d82ca07a617e22
Elee Lawleit <alibutt200235@gmail.com>
2024-04-01 22:01:13 +0500

    fix import issue ngIf wasn't being applied bc of missing CommonModule import

--- src/app/components/header/header.component.ts
+++ src/app/components/header/header.component.ts
@@ -3,11 +3,12 @@ import { ButtonComponent } from '../button/button.component';
 import { UiService } from '../../services/ui.service';
 import { Subscription } from 'rxjs';
 import { Router } from '@angular/router';
+import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'app-header',
   standalone: true,
-  imports: [ButtonComponent],
+  imports: [ButtonComponent, CommonModule],
   templateUrl: './header.component.html',
   styleUrl: './header.component.css',
 })
